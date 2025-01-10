4d71508e4debea9640a04d7f7a13fe9aaf133b93
Y.Kamoshita <78641260+line-0707@users.noreply.github.com>
2025-01-10 18:27:23 +0900

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+// Firebaseプロジェクトの設定
+const firebaseConfig = {
+    apiKey: "あなたのAPIキー",
+    authDomain: "あなたのプロジェクトID.firebaseapp.com",
+    projectId: "あなたのプロジェクトID",
+    storageBucket: "あなたのプロジェクトID.appspot.com",
+    messagingSenderId: "送信者ID",
+    appId: "アプリID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.icon
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
